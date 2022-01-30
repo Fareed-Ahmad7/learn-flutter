4d46c1d3c9e20cf3a3b99bb1aa5ae5dd4a0705c8
Fareed <90202062+Fareed-Ahmad7@users.noreply.github.com>
2022-01-30 15:37:24 +0530

    json mapping

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,24 +1,40 @@
-class CatalogModel{
-  static final items = [
-    Item(
-        id: 001,
-        name: "iPhone 13 Pro",
-        desc: "Apple iPhone 13th generation",
-        price: 999,
-        color: "#33505a",
-        image:
-            "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-pro-family-hero?wid=940&hei=1112&fmt=png-alpha&.v=1631220221000")
-  ];
+class CatalogModel {
+  static List<Item> items = [];
 }
 
-class Item{
+class Item {
   final int id;
   final String name;
-  final String desc; 
+  final String desc;
   final num price;
   final String color;
   final String image;
 
-  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
-}
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
 
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+        id: map["id"],
+        name: map["name"],
+        desc: map["desc"],
+        price: map["price"],
+        color: map["color"],
+        image: map["image"],
+    );  
+  }
+
+  toMap() => {
+    "id" : id,
+    "name" : name,
+    "desc" : desc,
+    "price" : price,
+    "color" : color,
+    "image" : image,
+  };
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,35 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_tutorial_catalog/models/catalog.dart';
 import 'package:flutter_tutorial_catalog/widgets/drawer.dart';
 import 'package:flutter_tutorial_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "fareed";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+        setState(() {
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(8, (index) => CatalogModel.items[0]);
 
     return Scaffold(
-      drawer: const Mydrawer(),
-      appBar: AppBar(
-        // backgroundColor: Colors.amber, 
-        // elevation: 0.0, ////shadowing
-        title: const Text("Catalog"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          // itemCount: CatalogModel.items.length,
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            // return ItemWidget(item: CatalogModel.items[index],);
-            return ItemWidget(item: dummyList[index],);
-          }
-        ,),
-      )
-    );
+        drawer: const Mydrawer(),
+        appBar: AppBar(
+          // backgroundColor: Colors.amber,
+          // elevation: 0.0, ////shadowing
+          title: const Text("Catalog"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)? ListView.builder(
+            // itemCount: CatalogModel.items.length,
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              // return ItemWidget(item: CatalogModel.items[index],);
+              return ItemWidget(
+                item: CatalogModel.items[index],
+              );
+            },
+          ):Center(child: CircularProgressIndicator(),),
+        ));
   }
 }
