2d29699dadef048a10411732058538c0ad888abe
Fareed <90202062+Fareed-Ahmad7@users.noreply.github.com>
2022-02-09 23:57:56 +0530

    added buy button in cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorial_catalog/widgets/themes.dart';
 import 'package:velocity_x/src/extensions/context_ext.dart';
+import 'package:velocity_x/src/extensions/num_ext.dart';
 import 'package:velocity_x/src/extensions/string_ext.dart';
+import 'package:velocity_x/src/flutter/padding.dart';
+import 'package:velocity_x/src/flutter/sizedbox.dart';
+import 'package:velocity_x/src/flutter/widgets.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -14,6 +18,36 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         // title: Text("CartPage"),
         title: "Cart".text.color(context.accentColor).make(),
+      ),
+      body: Column(
+        children: [
+          Placeholder().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center ,
+        children: [
+          "\$9999".text.xl5.color(context.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            child: "Buy".text.extraBold.black.make(),
+            style: ButtonStyle(backgroundColor: MaterialStateProperty.all(context.accentColor)),
+          ).w32(context)
+        ],
       ),
     );
   }
